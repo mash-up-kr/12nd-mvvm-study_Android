6e3d652bf587a099273260d68578fa44ecec6f9c
jaeryo2357 <jaeryo2357@naver.com>
2022-04-24 16:05:38 +0900

    :sparkles: implement main ui

--- 양민욱/app/src/main/java/com/mashup/mvvm/MainActivity.kt
+++ 양민욱/app/src/main/java/com/mashup/mvvm/MainActivity.kt
@@ -2,10 +2,19 @@ package com.mashup.mvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.mashup.mvvm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewBinding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(
+            LayoutInflater.from(this)
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(viewBinding.root)
     }
 }
