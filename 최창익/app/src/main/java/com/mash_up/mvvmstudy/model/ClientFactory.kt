85993f20f3af89493c9f4f2aeed84b1043a5c5a4
teddy-choe <aackddlr@gmail.com>
2022-04-27 01:20:07 +0900

    [refactor] ClientFactory 코드 정리

--- 최창익/app/src/main/java/com/mash_up/mvvmstudy/model/ClientFactory.kt
+++ 최창익/app/src/main/java/com/mash_up/mvvmstudy/model/ClientFactory.kt
@@ -1,5 +1,6 @@
 package com.mash_up.mvvmstudy.model
 
+import com.mash_up.mvvmstudy.BASE_URL
 import com.orhanobut.logger.Logger
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
@@ -10,37 +11,27 @@ object ClientFactory {
     private val okHttpClient = createOkHttpClient()
     private val retrofit = createRetrofit()
 
-    // TODO: Header 넣어야됨
-    private fun createOkHttpClient(): OkHttpClient {
-        val builder = OkHttpClient.Builder().apply {
+    private fun createOkHttpClient(): OkHttpClient =
+        OkHttpClient.Builder().apply {
             connectTimeout(8, TimeUnit.SECONDS)
             writeTimeout(8, TimeUnit.SECONDS)
             readTimeout(8, TimeUnit.SECONDS)
             addInterceptor { chain ->
-                val request = chain.request().newBuilder().addHeader("Accept", "application/vnd.github.v3+json").build()
-                chain.proceed(request)
+                val newRequest = chain.request().newBuilder()
+                    .addHeader("Accept", "application/vnd.github.v3+json").build()
+
+                Logger.i("Request's Header is : ${newRequest.headers()} From Url : ${newRequest.url()}")
+
+                chain.proceed(newRequest)
             }
-            addInterceptor { chain ->
-                val request = chain.request()
+        }.build()
 
-                Logger.i("Headers : ${request.headers()} From Url : ${request.url()}")
-
-                chain.proceed(request)
-            }
-        }
-
-        return builder.build()
-    }
-
-    private fun createRetrofit(): Retrofit {
-        val baseUrl = "https://api.github.com"
-
-        return Retrofit.Builder().apply {
-            baseUrl(baseUrl)
+    private fun createRetrofit(): Retrofit =
+        Retrofit.Builder().apply {
+            baseUrl(BASE_URL)
             addConverterFactory(GsonConverterFactory.create())
             client(okHttpClient)
         }.build()
-    }
 
     fun <T> createService(service: Class<T>): T {
         return retrofit.create(service)
