01406a88d264e65e4942447203d2a629e807a188
jaeryo2357 <jaeryo2357@naver.com>
2022-04-27 00:59:37 +0900

    :art: refactoring to api call to repository pattern

--- 양민욱/app/src/main/java/com/mashup/mvvm/extensions/ContextExt.kt
+++ 양민욱/app/src/main/java/com/mashup/mvvm/extensions/ContextExt.kt
@@ -2,8 +2,7 @@ package com.mashup.mvvm.extensions
 
 import android.content.Context
 import android.widget.Toast
-import androidx.annotation.StringRes
 
-fun Context.showToast(@StringRes messageRes: Int) {
-    Toast.makeText(this, messageRes, Toast.LENGTH_LONG).show()
+fun Context.showToast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
 }
--- 양민욱/app/src/main/java/com/mashup/mvvm/ui/main/MainActivity.kt
+++ 양민욱/app/src/main/java/com/mashup/mvvm/ui/main/MainActivity.kt
@@ -1,21 +1,15 @@
 package com.mashup.mvvm.ui.main
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doOnTextChanged
-import com.mashup.mvvm.R
 import com.mashup.mvvm.ServiceLocator
-import com.mashup.mvvm.data.error.CODE_SERVICE_NOT_AVAILABLE
+import com.mashup.mvvm.data.Response
+import com.mashup.mvvm.data.repository.GithubRepository
 import com.mashup.mvvm.databinding.ActivityMainBinding
-import com.mashup.mvvm.dto.RepositoriesDto
 import com.mashup.mvvm.extensions.showToast
-import com.mashup.mvvm.network.GithubApi
 import com.mashup.mvvm.ui.main.adapter.RepositoryAdapter
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     private val viewBinding: ActivityMainBinding by lazy {
@@ -24,7 +18,9 @@ class MainActivity : AppCompatActivity() {
         )
     }
     private val repositoryAdapter: RepositoryAdapter by lazy { RepositoryAdapter() }
-    private val githubApi: GithubApi by lazy { ServiceLocator.injectGithubApi() }
+    private val githubRepository: GithubRepository by lazy {
+        GithubRepository(ServiceLocator.injectGithubApi())
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -58,29 +54,24 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun fetchGithubRepository(query: String) {
-        githubApi.getRepositories(query).enqueue(
-            object : Callback<RepositoriesDto> {
-                override fun onResponse(
-                    call: Call<RepositoriesDto>,
-                    response: Response<RepositoriesDto>
-                ) {
-                    when {
-                        response.isSuccessful -> {
-                            response.body()?.run {
-                                repositoryAdapter.submitList(items)
-                            }
-                        }
-                        response.code() == CODE_SERVICE_NOT_AVAILABLE -> {
-                            showToast(messageRes = R.string.error_service_not_available)
+        Thread {
+            when (val data = githubRepository.getRepositories(query)) {
+                is Response.Success -> {
+                    viewBinding.rvRepository.post {
+                        repositoryAdapter.submitList(data.data.items)
+                    }
+                }
+                is Response.Error -> {
+                    viewBinding.root.post {
+                        data.message?.let { message ->
+                            showToast(message)
                         }
                     }
                 }
-
-                override fun onFailure(call: Call<RepositoriesDto>, t: Throwable) {
-                    Log.e(TAG, t.message ?: t.stackTraceToString())
+                else -> {
                 }
             }
-        )
+        }.start()
     }
 
     companion object {
