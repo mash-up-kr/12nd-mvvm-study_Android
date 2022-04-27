23d33a4af742175d93fde020ca234f9b9cc0bed2
teddy-choe <aackddlr@gmail.com>
2022-04-28 00:26:23 +0900

    [feature] image 로드 시 PlaceHolder로 매시업 이미지 적용 (포인트..)

--- 최창익/app/src/main/java/com/mash_up/mvvmstudy/BindingAdapter.kt
+++ 최창익/app/src/main/java/com/mash_up/mvvmstudy/BindingAdapter.kt
@@ -1,6 +1,7 @@
 package com.mash_up.mvvmstudy
 
 import android.widget.ImageView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
@@ -9,6 +10,8 @@ import com.bumptech.glide.request.RequestOptions
 fun loadImage(imageView: ImageView, url: String) {
     Glide.with(imageView.context)
         .load(url)
+        .placeholder(ContextCompat.getDrawable(imageView.context, R.drawable.mashup))
+        .error(ContextCompat.getDrawable(imageView.context, R.drawable.mashup))
         .apply(RequestOptions().fitCenter())
         .into(imageView)
 }
