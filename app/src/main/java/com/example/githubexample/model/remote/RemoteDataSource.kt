ee904aeda50985300aebdbeac3cf8c4219a10e78
jaeryo2357 <jaeryo2357@naver.com>
2022-05-01 14:47:07 +0900

    replace project

--- app/src/main/java/com/example/githubexample/model/remote/RemoteDataSource.kt
+++
@@ -1,7 +0,0 @@
-package com.example.githubexample.model.remote
-
-import com.example.githubexample.ui.MainContract
-
-interface RemoteDataSource {
-    fun getRepositoryList(query: String, modelResult: MainContract.Presenter.ModelResult)
-}
