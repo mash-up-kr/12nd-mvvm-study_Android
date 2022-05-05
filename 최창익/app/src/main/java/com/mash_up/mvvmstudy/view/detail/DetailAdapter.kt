9367f5ca719f5c387edd597d8d4dcb0f00b19640
teddy-choe <aackddlr@gmail.com>
2022-05-05 15:33:23 +0900

    [chore] DetailAdapter의 SealedClass간 diffCallback 추가

--- 최창익/app/src/main/java/com/mash_up/mvvmstudy/view/detail/DetailAdapter.kt
+++ 최창익/app/src/main/java/com/mash_up/mvvmstudy/view/detail/DetailAdapter.kt
@@ -5,13 +5,16 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.mash_up.mvvmstudy.databinding.*
+import com.mash_up.mvvmstudy.databinding.ItemDescriptionBinding
+import com.mash_up.mvvmstudy.databinding.ItemLanguageBinding
+import com.mash_up.mvvmstudy.databinding.ItemLastUpdatedBinding
+import com.mash_up.mvvmstudy.databinding.ItemProfileBinding
 import com.mash_up.mvvmstudy.repository.model.DetailFeed
 
 class DetailAdapter :
     ListAdapter<DetailFeed, RecyclerView.ViewHolder>(detailFeedDiffCallback) {
     override fun getItemViewType(position: Int): Int {
-        return when(getItem(position)) {
+        return when (getItem(position)) {
             is DetailFeed.Profile -> 0
             is DetailFeed.Description -> 1
             is DetailFeed.Language -> 2
@@ -22,7 +25,7 @@ class DetailAdapter :
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
 
-        return when(viewType) {
+        return when (viewType) {
             0 -> ProfileViewHolder(ItemProfileBinding.inflate(inflater, parent, false))
             1 -> DescriptionViewHolder(ItemDescriptionBinding.inflate(inflater, parent, false))
             2 -> LanguageViewHolder(ItemLanguageBinding.inflate(inflater, parent, false))
@@ -74,13 +77,29 @@ class DetailAdapter :
     }
 
     companion object {
-        //TODO: Sealed Class 간 비교는 어떻게 하지?
+        //TODO: Item에 id 값이 없는 경우 areContentTheSame과 areItemsTheSame이 같아지는 느낌..?
         private val detailFeedDiffCallback = object : DiffUtil.ItemCallback<DetailFeed>() {
-            override fun areItemsTheSame(oldItem: DetailFeed, newItem: DetailFeed): Boolean =
-                oldItem == newItem
+            override fun areItemsTheSame(oldItem: DetailFeed, newItem: DetailFeed): Boolean {
+                when {
+                    oldItem is DetailFeed.Profile && newItem is DetailFeed.Profile && oldItem == newItem -> return true
+                    oldItem is DetailFeed.Description && newItem is DetailFeed.Description && oldItem == newItem -> return true
+                    oldItem is DetailFeed.Language && newItem is DetailFeed.Language && oldItem == newItem -> return true
+                    oldItem is DetailFeed.LastUpdated && newItem is DetailFeed.LastUpdated && oldItem == newItem -> return true
+                }
 
-            override fun areContentsTheSame(oldItem: DetailFeed, newItem: DetailFeed): Boolean =
-                oldItem == newItem
+                return false
+            }
+
+            override fun areContentsTheSame(oldItem: DetailFeed, newItem: DetailFeed): Boolean {
+                when {
+                    oldItem is DetailFeed.Profile && newItem is DetailFeed.Profile && oldItem == newItem -> return true
+                    oldItem is DetailFeed.Description && newItem is DetailFeed.Description && oldItem == newItem -> return true
+                    oldItem is DetailFeed.Language && newItem is DetailFeed.Language && oldItem == newItem -> return true
+                    oldItem is DetailFeed.LastUpdated && newItem is DetailFeed.LastUpdated && oldItem == newItem -> return true
+                }
+
+                return false
+            }
         }
     }
 }
