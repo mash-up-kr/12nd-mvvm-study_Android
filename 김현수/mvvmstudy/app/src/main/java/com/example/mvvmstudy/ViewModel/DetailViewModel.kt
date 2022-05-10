10edc7a72b03d4e4e34bba5024471e63c0fb94a4
Kim Hyun Su <86879099+KimHance@users.noreply.github.com>
2022-05-10 12:43:30 +0900

    리뷰 반영
    DetailViewModel 수정
    
    Co-authored-by: jaeryo2357 <33657541+jaeryo2357@users.noreply.github.com>

--- 김현수/mvvmstudy/app/src/main/java/com/example/mvvmstudy/ViewModel/DetailViewModel.kt
+++ 김현수/mvvmstudy/app/src/main/java/com/example/mvvmstudy/ViewModel/DetailViewModel.kt
@@ -9,13 +9,5 @@ import com.example.mvvmstudy.data.Detail
 
 class DetailViewModel(savedStateHandle: SavedStateHandle) : ViewModel() {
 
-    private val _detail: Detail? = savedStateHandle.get<Detail>("detail")
-
-    private val _ui: MutableLiveData<Detail> = MutableLiveData()
-    val ui: LiveData<Detail>
-        get() = _ui
-
-    init {
-        _ui.value = _detail!!
-    }
+   val detail = savedStateHandle.getLiveData<Detail>("detail")
 }
