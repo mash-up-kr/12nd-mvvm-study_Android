2b58975cc0d5a47c6156e0aea50435c1d7882195
teddy-choe <aackddlr@gmail.com>
2022-05-15 15:33:49 +0900

    [chore] UnitTest 코드 일부 수정

--- 최창익/app/src/test/java/com/mash_up/mvvmstudy/RemoteTest.kt
+++ 최창익/app/src/test/java/com/mash_up/mvvmstudy/RemoteTest.kt
@@ -69,7 +69,10 @@ class RemoteTest {
                             login = "dtrupenn",
                             avatarUrl = "https://secure.gravatar.com/avatar/e7956084e75f239de85d3a31bc172ace?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png"
                         ),
-                        language = "Assembly"
+                        language = "Assembly",
+                        description = "A C implementation of Tetris using Pennsim through LC4",
+                        stargazersCount = 1,
+                        updatedAt = "2013-01-05T17:58:47Z"
                     )
                 )
             )
