1060a2c9c614b7d5698b291338b63605c6ae5cb1
hj1115hj <hyejin_one@naver.com>
2022-05-19 02:36:15 +0900

    feature: Apply coroutine and flow

--- hyejin/app/src/main/java/com/joocoding/android/app/githubsearch/model/GithubRepository.kt
+++ hyejin/app/src/main/java/com/joocoding/android/app/githubsearch/model/GithubRepository.kt
@@ -1,37 +1,28 @@
 package com.joocoding.android.app.githubsearch.model
 
 import com.joocoding.android.app.githubsearch.data.dto.RepositoriesDto
-import com.joocoding.android.app.githubsearch.model.error.CODE_SERVICE_NOT_AVAILABLE
 import com.joocoding.android.app.githubsearch.network.ClientFactory
 import com.joocoding.android.app.githubsearch.network.SearchService
-
-import java.io.IOException
 
 class GithubRepository {
     private val githubApi: SearchService = ClientFactory.createService(SearchService::class.java)
 
-    fun getRepositories(query: String): Response<RepositoriesDto>? {
-        return try {
-            val response = githubApi.getRepository(query).execute()
+    suspend fun getRepositories(query: String): Response<RepositoriesDto> {
+        val response = githubApi.getRepository(query)
 
-            when {
-                response.isSuccessful -> {
-                    response.body()?.run {
-                        Response.Success(
-                            code = response.code(),
-                            data = this
-                        )
-                    }
-                }
-                response.code() == CODE_SERVICE_NOT_AVAILABLE -> {
-                    Response.Error(
-                        throwable = IllegalStateException("현재 api 서버가 불안정합니다.")
-                    )
-                }
-                else -> Response.Error()
+        return when {
+            response.isSuccessful -> {
+                Response.Success(
+                    code = response.code(),
+                    data = response.body()
+                )
             }
-        } catch (e: IOException) {
-            null
+            else -> Response.Error(
+                code = response.code(),
+                message = response.message()
+
+            )
+
         }
     }
 
--- hyejin/app/src/main/java/com/joocoding/android/app/githubsearch/viewmodel/MainViewModel.kt
+++ hyejin/app/src/main/java/com/joocoding/android/app/githubsearch/viewmodel/MainViewModel.kt
@@ -1,42 +1,58 @@
 package com.joocoding.android.app.githubsearch.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.joocoding.android.app.githubsearch.model.Event
 import com.joocoding.android.app.githubsearch.model.GithubRepository
 import com.joocoding.android.app.githubsearch.model.Response
 import com.joocoding.android.app.githubsearch.model.data.Repository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
 
 class MainViewModel : ViewModel() {
 
     private val githubRepository = GithubRepository()
 
-    private val _repositories = MutableLiveData<List<Repository>>()
-    val repositories: LiveData<List<Repository>> = _repositories
+    private val _repositories = MutableStateFlow<List<Repository>>(emptyList())
+    val repositories: StateFlow<List<Repository>> = _repositories.asStateFlow()
 
     private val _toastMessage = MutableLiveData<Event<String>>()
     val toastMessage: LiveData<Event<String>> = _toastMessage
 
     fun getRepository(query: String = "") {
-        Thread {
-            when (val data = githubRepository.getRepositories(query)) {
-                is Response.Success -> {
-                    _repositories.postValue(data.data.repositories)
-                }
-                is Response.Error -> {
-                    data.message?.let { message ->
-                        _toastMessage.postValue(Event(message))
+        viewModelScope.launch {
+            runCatching {
+                githubRepository.getRepositories(query)
+
+            }.onSuccess { data ->
+                when (data) {
+                    is Response.Success -> {
+                        _repositories.value = data.data?.repositories ?: emptyList()
                     }
+                    is Response.Error -> {
+                        data.message?.let { message ->
+                            _toastMessage.postValue(Event(message))
+                        }
+                    }
+
                 }
-                else -> {
-                }
+
+            }.onFailure { e ->
+                Log.e("getRepository", e.toString())
             }
-        }.start()
+
+
+        }
     }
-
-
 }
+
+
+
 
 
