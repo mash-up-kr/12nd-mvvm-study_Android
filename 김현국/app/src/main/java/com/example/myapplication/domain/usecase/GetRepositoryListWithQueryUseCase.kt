8d7cfe36c5554c584b2ae00528752bf2f51c13cc
boris <boris0815@naver.com>
2022-05-24 15:22:55 +0900

    Update: Repository 의존성 주입
     Update: Repository 의존성 주입

--- 김현국/app/src/main/java/com/example/myapplication/domain/usecase/GetRepositoryListWithQueryUseCase.kt
+++ 김현국/app/src/main/java/com/example/myapplication/domain/usecase/GetRepositoryListWithQueryUseCase.kt
@@ -4,12 +4,13 @@ import com.example.myapplication.domain.Results
 import com.example.myapplication.domain.model.DomainRepository
 import com.example.myapplication.domain.repository.SearchRepoRepository
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
 /**
  * @author 김현국
  * @created 2022/05/02
  */
-class GetRepositoryListWithQueryUseCase(
+class GetRepositoryListWithQueryUseCase @Inject constructor(
     private val repository: SearchRepoRepository
 ) {
     operator fun invoke(query: String): Flow<Results<List<DomainRepository>>> {
