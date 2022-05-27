50806d39ed66b7bece94a4c837cc71abd6a15f6d
hj1115hj <hyejin_one@naver.com>
2022-05-28 01:27:14 +0900

    apply hilt

---
+++ hyejin/app/src/main/java/com/joocoding/android/app/githubsearch/MyApplication.kt
@@ -0,0 +1,7 @@
+package com.joocoding.android.app.githubsearch
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication : Application()
--- hyejin/app/src/main/java/com/joocoding/android/app/githubsearch/viewmodel/DetailViewModel.kt
+++ hyejin/app/src/main/java/com/joocoding/android/app/githubsearch/viewmodel/DetailViewModel.kt
@@ -5,9 +5,11 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.joocoding.android.app.githubsearch.constant.KEY_REPOSITORY
 import com.joocoding.android.app.githubsearch.model.data.Detail
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-
-class DetailViewModel(
+@HiltViewModel
+class DetailViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle
 ) : ViewModel() {
     val detail: LiveData<Detail> = savedStateHandle.getLiveData(KEY_REPOSITORY)
