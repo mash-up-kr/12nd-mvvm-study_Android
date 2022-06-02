ae7680f9f99fb11e737633d5daa02345faf498fd
sooziini <soojinpar1026@gmail.com>
2022-06-03 01:11:49 +0900

    feat: BindingAdapter bindImage url null 처리 추가 및 placeholder 설정

--- 박수진/week1/app/src/main/java/com/example/week1/presentation/view/BindingAdapter.kt
+++ 박수진/week1/app/src/main/java/com/example/week1/presentation/view/BindingAdapter.kt
@@ -1,16 +1,26 @@
 package com.example.week1.presentation.view
 
+import android.graphics.drawable.Drawable
 import android.widget.ImageView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.example.week1.R
 
 object BindingAdapter {
 
-    @BindingAdapter("bindImage")
     @JvmStatic
-    fun bindImage(imageView: ImageView, url: String) {
+    @BindingAdapter(
+        value = ["imageUrl", "imagePlaceHolder"],
+        requireAll = false      // 모든 속성이 없어도 됨
+    )
+    fun bindImage(imageView: ImageView, url: String?, placeholder: Drawable?) {
+        val ph = placeholder ?: ContextCompat.getDrawable(imageView.context, R.drawable.ic_default)
+
         Glide.with(imageView.context)
             .load(url)
+            .placeholder(ph)
+            .error(ph)
             .into(imageView)
     }
 }
